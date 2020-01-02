43ee4861c3cce4b2f1292f1eb807bd819e80c7b6
p4vlowVl4d <p4vlow.vl4d@yandex.ru>
2020-01-02 20:38:45 +0300

    fix templates

--- web/handler.go
+++ web/handler.go
@@ -9,14 +9,20 @@ import (
 func NewRouter() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/", index)
+	r.Get("/login", auth)
 	r.Handle("/assets/*",
-		http.StripPrefix("/assets",
+		http.StripPrefix("/assets/",
 			http.FileServer(http.Dir("public/"))))
 	return r
 }
 
 func index(w http.ResponseWriter, r *http.Request)  {
-	tmpl, _ := template.ParseFiles("public/template/index.html")
+	tmpl, _ := template.ParseFiles("resources/view/index.html")
 	tmpl.Execute(w, nil)
 	//fmt.Fprintf(w, "New web Panel.\nYou Ip: %s", r.RemoteAddr)
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("resources/view/security/login.html")
+	tmpl.Execute(w, nil)
 }
